17045985ab0b6f4a352c00faf6f7ecf63332bc98
猪野 湧斗 <ice.softicon30th@gmail.com>
2016-04-07 23:37:17 +0900

    リファクタリングっぽいこと

--- access.js
+++ access.js
@@ -57,40 +57,44 @@ function bypass(req, res, proxyHost, forwardURL) {
     
     var forward = request({ uri: forwardURL, gzip: true });
     
-    function initPiping() {
-        res.removeHeader('Content-Length');
-        res.removeHeader('Content-Encoding');
-        res.setHeader('Transfer-Encoding', 'chunked');
-    }
-    
     function allowAccess() {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Content-Security-Policy', 'connect-src *');
     }
     
+    function headerOverride(response) {
+        for (var name in response.headers)
+            res.setHeader(name, response.headers[name]);
+    }
+    
     forward.on('response', function (response) {
         
         if (res.headersSent) {
-            forwardResponse.pipe(res); return;
+            forwardResponse.pipe(res);
+            return;
         }
         
-        for (var name in response.headers) {
-            res.setHeader(name, response.headers[name]);
-        }
-        
+        headerOverride(response);
         allowAccess();
         
         var contentType = response.headers['content-type'] || '';
         
-        if (contentType.match('text/html')) {
-            initPiping();
-            var charsetConverter = HTMLCharset(contentType);
-            var urlConverter = HTMLUrlConverter(proxyHost, forwardURL);
-            forward.pipe(charsetConverter).pipe(urlConverter).pipe(res);
-        } else if (contentType.match('text/css')) {
-            initPiping();
-            var charsetConverter = CSSCharset(contentType);
-            var urlConverter = CSSUrlConverter(proxyHost, forwardURL);
+        var documentRegexp = /(text\/html|text\/css)/.exec(contentType);
+        if (documentRegexp) {
+            res.removeHeader('Content-Length');
+            res.removeHeader('Content-Encoding');
+            res.setHeader('Transfer-Encoding', 'chunked');
+            
+            var charsetConverter, urlConverter;
+            
+            if (documentRegexp[0] == 'text/html') {
+                charsetConverter = HTMLCharset(contentType);
+                urlConverter = HTMLUrlConverter(proxyHost, forwardURL);
+            } else {
+                charsetConverter = CSSCharset(contentType);
+                urlConverter = CSSUrlConverter(proxyHost, forwardURL);    
+            }
+            
             forward.pipe(charsetConverter).pipe(urlConverter).pipe(res);
         } else {
             response.pipe(res);
