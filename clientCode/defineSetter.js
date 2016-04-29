feee942a6bac8561788fa9daa40abd108594c1ee
Yuto Ino <ice.softicon30th@gmail.com>
2016-04-30 00:17:24 +0900

    デザイン中

--- clientCode/defineSetter.js
+++ clientCode/defineSetter.js
@@ -1,11 +1,11 @@
 (function () {
     // Element のプロトタイプに innerHTML Setter を取得する
-    var nativeLinkHref = Object.getOwnPropertyDescriptor(HTMLLinkElement.prototype, 'href').setter;
+    var nativeLinkHref = Object.getOwnPropertyDescriptor(HTMLScriptElement.prototype, 'src').setter;
 
     // Element のプロトタイプに innerHTML Setter を設定する
-    Object.defineProperty(HTMLLinkElement.prototype, 'href', {
+    Object.defineProperty(HTMLScriptElement.prototype, 'src', {
         setter: function(html) {
-            console.log(this.tagName + 'の href が設定されました！');
+            console.log(this.tagName + 'の src が設定されました！');
             return nativeLinkHref.call(this, html);
         }
     });
